d5ace21b2e66597b8c9bbfd7509ee804329aee5f
Peko-Chama <99276653+AleMar21430@users.noreply.github.com>
2023-08-29 12:02:12 -0600

    Update DPLL.py

--- DPLL.py
+++ DPLL.py
@@ -1,47 +1,77 @@
 def DPLL(B, I):
-    if not B:  # Si no hay cláusulas restantes, la fórmula es satisfacible
-        return True, I
-    
-    for clause in B:
-        if not clause:  # Si alguna cláusula está vacía, la fórmula no es satisfacible
-            return False, None
-    
-    # Seleccionar una literal L (aquí se selecciona el primer literal de la primera cláusula)
-    L = next(iter(B[0]))
+	if not B:  # Si no hay cláusulas restantes, la fórmula es satisfacible
+		return True, I
+	
+	for clause in B:
+		if not clause:  # Si alguna cláusula está vacía, la fórmula no es satisfacible
+			return False, None
+	
+	# Seleccionar una literal L (aquí se selecciona el primer literal de la primera cláusula)
+	L = next(iter(B[0]))
 
-    # Crear B' eliminando cláusulas con L y complementos de L
-    _B = [clause - {-L} for clause in B if L in clause]
-    
-    # Crear B'' eliminando cláusulas con complemento de L y L
-    _B = [clause - {L} for clause in B if -L in _B]
+	# Crear B' eliminando cláusulas con L y complementos de L
+	B_true = [clause - {-L} for clause in B if L in clause]
+	
+	# Crear B'' eliminando cláusulas con complemento de L y L
+	B_false = [clause - {L} for clause in B if -L in clause]
 
-    # Verificar que el tamaño de B disminuya en cada llamada recursiva
-    if len(_B) >= len(B):
-        return False, None
+	# Verificar que el tamaño de B disminuya en cada llamada recursiva
+	if len(B_true) >= len(B) or len(B_false) >= len(B):
+		return False, None
 
-    # Asignar L como verdadero en la asignación parcial I
-    I_true = I.copy()
-    I_true[abs(L)] = True
+	# Asignar L como verdadero en la asignación parcial I
+	I_true = I.copy()
+	I_true[abs(L)] = True
 
-    # Llamada recursiva con L verdadero
-    result_true, assignment_true = DPLL(_B, I_true)
-    if result_true:
-        return True, assignment_true
+	# Llamada recursiva con L verdadero
+	result_true, assignment_true = DPLL(B_true, I_true)
+	if result_true:
+		return True, assignment_true
 
-    return False, None
+	# Asignar L como falso en la asignación parcial I
+	I_false = I.copy()
+	I_false[abs(L)] = False
 
+	# Llamada recursiva con L falso
+	result_false, assignment_false = DPLL(B_false, I_false)
+	if result_false:
+		return True, assignment_false
 
+	return False, None
+
+# Ejemplo de uso
 if __name__ == "__main__":
-    # Representación de la fórmula en forma de cláusulas (conjuntos de literales)
-    B = [
-        {1} ,
-        {-1} 
-    ]
-    I = {}
+	# Representación de la fórmula en forma de cláusulas (conjuntos de literales)
 
-    result, assignment = DPLL(B, I)
+	EXPR = "{p},{q},{-p}"
 
-    if result:
-        print("La fórmula es satisfacible. Asignación parcial:", assignment)
-    else:
-        print("La fórmula no es satisfacible.")
+	elements = {}
+	curr_Index = 1
+
+	B = []
+
+	for e in EXPR.split(','):
+		el = e.strip('{}')
+		if el.startswith('-'):
+			if el.strip('-') in elements:
+				elements[el] = -elements[el.strip('-')]
+				B.append({-elements[el.strip('-')]})
+			else:
+				elements[el] = -curr_Index
+				B.append({-curr_Index})
+				curr_Index += 1
+		else:
+			elements[el] = curr_Index
+			B.append({curr_Index})
+			curr_Index += 1
+
+	print(B)
+
+	I = {}
+
+	result, assignment = DPLL(B, I)
+
+	if result:
+		print("La fórmula es satisfacible. Asignación parcial:", assignment)
+	else:
+		print("La fórmula no es satisfacible.")
