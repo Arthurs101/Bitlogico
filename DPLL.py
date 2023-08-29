971b661cf391678686f95af16f41f91e82595998
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2023-08-29 14:50:39 -0600

    fixed dpll method, parser needs fixing still

--- DPLL.py
+++ DPLL.py
@@ -9,16 +9,10 @@ def DPLL(B, I):
 	# Seleccionar una literal L (aquí se selecciona el primer literal de la primera cláusula)
 	L = next(iter(B[0]))
 
-	# Crear B' eliminando cláusulas con L y complementos de L
-	_B = [clause - {-L} for clause in B if L in clause]
-	
-	# Crear B'' eliminando cláusulas con complemento de L y L
-	_B = [clause - {L} for clause in B if -L in _B]
-
-	# Verificar que el tamaño de B disminuya en cada llamada recursiva
-	if len(_B) >= len(B):
-		return False, None
-
+	#asignacion con true
+	# Crear B' eliminando cláusulas con L y complementos de L 
+	_B = [clause for clause in B if abs(L) not in clause]
+	_B = [clause.difference({-abs(L)}) for clause in _B]
 	# Asignar L como verdadero en la asignación parcial I
 	I_true = I.copy()
 	I_true[abs(L)] = True
@@ -27,11 +21,13 @@ def DPLL(B, I):
 	result_true, assignment_true = DPLL(_B, I_true)
 	if result_true:
 		return True, assignment_true
-
-	# Asignar L como falso en la asignación parcial I
+	
+	# asignacion con false
+	_B = [clause for clause in B if -abs(L) not in clause]
+	_B = [clause.difference({abs(L)}) for clause in _B]
+	# Asignar L como verdadero en la asignación parcial I
 	I_false = I.copy()
 	I_false[abs(L)] = False
-
 	# Llamada recursiva con L falso
 	result_false, assignment_false = DPLL(_B, I_false)
 	if result_false:
@@ -43,7 +39,7 @@ def DPLL(B, I):
 if __name__ == "__main__":
 	# Representación de la fórmula en forma de cláusulas (conjuntos de literales)
 
-	EXPR = "{p,-q},{p,r}"
+	EXPR = "{{p,-q},{-p,-r}}"
 
 	elements = {}
 	curr_Index = 1
@@ -69,10 +65,10 @@ if __name__ == "__main__":
 				B.append({curr_Index})
 				curr_Index += 1
 
-	print(B)
-	
+	# print(B)
+	_b = [{-1,-2},{-1,3}]
 	I = {}
-	result, assignment = DPLL(B, I)
+	result, assignment = DPLL(_b, I)
 
 	if result:
 		print("La fórmula es satisfacible. Asignación parcial:", assignment)
